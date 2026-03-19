f8af916e32c261fb78f20e72db70c7d0aae2b5ad
zhangqb <zhangqb@bsfit.com.cn>
2026-03-19 14:04:01 +0800

    删除无意义的dylib复制命令

--- build.py
+++ build.py
@@ -403,13 +403,8 @@ def build_deb_from_folder(version, binary_folder):
 
 def build_flutter_dmg(version, features):
     if not skip_cargo:
-        # set minimum osx build target, now is 10.14, which is the same as the flutter xcode project
-        system2(
-            f'MACOSX_DEPLOYMENT_TARGET=10.14 cargo build --features {features} --release')
-    # copy dylib
-    system2(
-        "cp target/release/libzdesk.dylib target/release/libzdesk.dylib")
-    os.chdir('flutter')
+        system2("cargo build --release --features " + features + " --lib")
+    # dylib is already named libzdesk.dylib by cargo,    os.chdir('flutter')
     system2('flutter build macos --release')
     system2('cp -rf ../target/release/service ./build/macos/Build/Products/Release/RustDesk.app/Contents/MacOS/')
     '''
